1c46ffbd56d6754f160ac97b3bc8ae5da0b17603
irinakohna <irinakokhna@mail.ru>
2021-09-21 21:36:46 +0300

    changes 2

--- src/test/java/guru/qa/tests/Homework.java
+++ src/test/java/guru/qa/tests/Homework.java
@@ -31,15 +31,7 @@ public class Homework {
         $(By.xpath("//*[@id=\"city\"]/div/div[2]")).click();
         $(byText("Delhi")).click();
         $(By.xpath("//*[@id=\"submit\"]")).click();
-        $(By.xpath("//div[4]/div/div/div[2]/div/table/tbody/tr[1]/td[2]")).shouldHave(text("Irina Krbtv"));
-        $(By.xpath("//div[4]/div/div/div[2]/div/table/tbody/tr[2]/td[2]")).shouldHave(text("krbtvir@mail.com"));
-        $(By.xpath("//div[4]/div/div/div[2]/div/table/tbody/tr[3]/td[2]")).shouldHave(text("Female"));
-        $(By.xpath("//div[4]/div/div/div[2]/div/table/tbody/tr[4]/td[2]")).shouldHave(text("8005553535"));
-        $(By.xpath("//div[4]/div/div/div[2]/div/table/tbody/tr[5]/td[2]")).shouldHave(text("9 January,1990"));
-        $(By.xpath("//div[4]/div/div/div[2]/div/table/tbody/tr[6]/td[2]")).shouldHave(text("Maths"));
-        $(By.xpath("//div[4]/div/div/div[2]/div/table/tbody/tr[7]/td[2]")).shouldHave(text("Music"));
-        $(By.xpath("//div[4]/div/div/div[2]/div/table/tbody/tr[8]/td[2]")).shouldHave(text("Screenshot 2021-09-02 at 12.26.43.png"));
-        $(By.xpath("//div[4]/div/div/div[2]/div/table/tbody/tr[9]/td[2]")).shouldHave(text("addressless"));
-        $(By.xpath("//div[4]/div/div/div[2]/div/table/tbody/tr[10]/td[2]")).shouldHave(text("NCR Delhi"));
+        $(".table-responsive").shouldHave(text("Irina Krbtv"), text("krbtvir@mail.com"),
+                text ("Screenshot 2021-09-02 at 12.26.43.png"), text("Maths"), text("NCR Delhi"));
     }
 }
