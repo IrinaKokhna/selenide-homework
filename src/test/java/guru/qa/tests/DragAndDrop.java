08380ba2ae056d91e0d5801dcd2be9f54273c287
irinakohna <irinakokhna@mail.ru>
2021-09-22 22:57:55 +0300

    rename

--- src/test/java/guru/qa/tests/DragAndDrop.java
+++ src/test/java/guru/qa/tests/DragAndDrop.java
@@ -6,7 +6,7 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class DragAndDrop {
     @Test
-    void draganddrop () {
+    void dragAndDrop() {
         open("https://the-internet.herokuapp.com/drag_and_drop");
         $("#column-b").dragAndDropTo($("#column-a"));
         $("#column-a").shouldHave(text("B"));
